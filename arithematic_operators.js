9235d603af66997cba8ff854fe4903049cfe2722
mounika <mounikamusham12@gmail.com>
2021-12-13 17:49:25 +0530

    aritematic-operations

---
+++ arithematic_operators.js
@@ -0,0 +1,49 @@
+//addition
+
+var x=10;
+var y=30;
+var p=40;
+var z=x+y+p;
+console.log(z);
+
+//substraction
+
+var x=30;
+var y=10;
+var p=5;
+var z=x-y-p;
+console.log(z);
+
+//multiplication
+
+var x=10;
+var y=30;
+var p=5;
+var z=x*y*p;
+console.log(z);
+
+//
+
+var x=10;
+var y=30;
+var p=5;
+var z=x/y/p;
+console.log(z);
+
+//modulus
+
+var x=10;
+var y=30;
+var p=5;
+var z=x%y%p;
+console.log(z);
+
+//all
+
+var x=10;
+var y=30;
+var b=20;
+var c=15;
+var d=35;
+var z=x+y-b/c*d;
+console.log(z);
